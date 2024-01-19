3c56cc87e591c0c6643177d158dcb3cdc6d8192a
Arthur Lenne <arthur.lenne@grenoble-inp.org>
2024-01-19 12:08:42 +0100

    [Non fonctionnel] Début emalloc_small

--- src/mem_small.c
+++ src/mem_small.c
@@ -8,11 +8,26 @@
 #include "mem.h"
 #include "mem_internals.h"
 
-void *
-emalloc_small(unsigned long size)
+void * emalloc_small(unsigned long size)
 {
-    /* ecrire votre code ici */
-    return (void *)0;
+    if (arena.chunkpool == NULL){
+        unsigned long tailleOctet = mem_realloc_small();
+        void* ptr_small_mem = arena.chunkpool;
+        
+        // écrit les adresses pour la listes !
+        do {
+            (*ptr_small_mem) = ptr_small_mem+96;
+            ptr_small_mem += 96; // Bouge de 96 octets
+            tailleOctet -= 96:
+        } while( tailleOctet > 96 );
+        //mark_memarea_and_get_user_ptr(ptr_small_mem,96,SMALL_KIND);
+    }
+
+    // Chunk pool valide, on prend le 1er !
+    void* ptr_chunk_start = arena.chunkpool;
+    arena.chunkpool = (*ptr_chunk_start);
+
+    return mark_memarea_and_get_user_ptr(ptr_chunk_start,96,SMALL_KIND);
 }
 
 void efree_small(Alloc a) {
