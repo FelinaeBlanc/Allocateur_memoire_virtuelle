1b5e2026f6ecf58f7f660f35d94ac347641a4467
Arthur Lenne <arthur.lenne@grenoble-inp.org>
2024-01-31 12:40:18 +0100

    [PAS FINIT] Début allocation moyenne

--- src/mem_medium.c
+++ src/mem_medium.c
@@ -8,6 +8,7 @@
 #include <assert.h>
 #include "mem.h"
 #include "mem_internals.h"
+#include <math.h>
 
 unsigned int puiss2(unsigned long size) {
     unsigned int p=0;
@@ -22,12 +23,33 @@ unsigned int puiss2(unsigned long size) {
 }
 
 
-void *
-emalloc_medium(unsigned long size)
+void * emalloc_medium(unsigned long size)
 {
     assert(size < LARGEALLOC);
     assert(size > SMALLALLOC);
-    /* ecrire votre code ici */
+    
+    // Prend en compte le marquage
+    unsigned long sizeFinal = size + 32;
+    // calcule l’indice de la TZL contenant la bonne taille 
+    unsigned int minIndice = puiss2(sizeFinal); 
+    uint32_t maxIndice = (FIRST_ALLOC_MEDIUM_EXPOSANT + arena.medium_next_exponant);
+
+    unsigned int minUsable = minIndice;
+    while(arena.TZL[minUsable] == NULL && minUsable < (maxIndice) ){
+        minUsable ++;
+    }
+    if (minUsable > maxIndice){ // Si on a même pas de tailleMax alloué
+        mem_realloc_medium();
+        minUsable--; // Recentre à la case de l'indice max !
+    }
+
+    for (uint32_t i=minUsable;i>minIndice;i--){
+        // Faire couper en 2 ! + on le met en dessous !
+    }
+    
+    // Si un bloc est disponible, il est marqué et l’adresse utilisateur est retournée
+    
+
     return (void *) 0;
 }
 
